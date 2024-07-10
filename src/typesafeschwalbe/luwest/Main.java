1c59c86a2fd3f8a82b873690f5156cf1d8e4ebb7
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-07-10 12:11:04 +0300

    Write normal code for once

--- src/typesafeschwalbe/luwest/Main.java
+++ src/typesafeschwalbe/luwest/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             Engine.init("Luwest");
-            Engine.setScene(Overworld.SCENE.get());
+            Engine.setScene(Overworld.createScene());
             Engine.start();
         } catch(Exception e) {
             Main.handleCrash(e);
--- src/typesafeschwalbe/luwest/scenes/Overworld.java
+++ src/typesafeschwalbe/luwest/scenes/Overworld.java
@@ -1,8 +1,6 @@
 
 package typesafeschwalbe.luwest.scenes;
 
-import java.util.function.Supplier;
-
 import typesafeschwalbe.luwest.engine.Engine;
 import typesafeschwalbe.luwest.engine.Entity;
 import typesafeschwalbe.luwest.engine.Scene;
@@ -13,13 +11,17 @@ public class Overworld {
         float value = 0;
     }
 
-    public static Supplier<Scene> SCENE = () -> new Scene("overworld.json")
-        .with(new Entity().with(Hunger.class, new Hunger()))
-        .with(scene -> {
-            for(Entity e: scene.allWith(Hunger.class)) {
-                Hunger h = e.get(Hunger.class);
-                h.value += Engine.deltaTime();
-            }
-        });
+    public static void decreaseHunger(Scene scene) {
+        for(Entity e: scene.allWith(Hunger.class)) {
+            Hunger h = e.get(Hunger.class);
+            h.value += Engine.deltaTime();
+        }
+    }
+
+    public static Scene createScene() {
+        return new Scene("overworld.json")
+            .with(new Entity().with(Hunger.class, new Hunger()))
+            .with(Overworld::decreaseHunger);
+    }
 
 }
